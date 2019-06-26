28fc52a4ce6fa518ad39e976452c5ac4ca6b6996
Moroz Vladislav <nikeblack97@gmail.com>
2019-06-26 17:31:41 +0300

    fixed bug with searching in another counties

--- src/app/services/nestoria-service.service.ts
+++ src/app/services/nestoria-service.service.ts
@@ -13,10 +13,17 @@ export class NestoriaService {
   private baseUtl = 'https://api.nestoria';
   private items$: BehaviorSubject = new BehaviorSubject([]);
   constructor( private http: HttpClient) {}
+  private getBaseUrl(country: string ): string{
+    switch( country ){
+      case 'uk': return 'https://api.nestoria.co.' + country;
+      case 'br': case 'au': return 'https://api.nestoria.com.'+country;
+      default: return 'https://api.nestoria.' + country;
+    }
+  }
   public get( searchParams: ISearchData ): Observable {
-    this.items$.next(this.http.jsonp('https://api.nestoria.co.uk/'
-      + 'api?encoding=json&pretty=1&action=search_listings&'
-      + 'country='+ searchParams.selectedCountry
+    this.items$.next(this.http.jsonp( this.getBaseUrl(searchParams.selectedCountry)
+      + '/api?encoding=json&pretty=1&action=search_listings&'
+      + 'country=' + searchParams.selectedCountry
       + '&listing_type=buy&place_name='
       + searchParams.cityName
       , 'callback').pipe( map( res => res.response.listings ) ));
